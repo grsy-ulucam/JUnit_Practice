f88cb326dd97f4e8ef4a43a0617223e2e841ee88
grsy-ulucam <emre.yucel48.45@gmail.com>
2023-08-24 15:39:50 +0300

    24.08.2023

--- src/test/java/tasks/Task01.java
+++ src/test/java/tasks/Task01.java
@@ -1,9 +1,14 @@
 package tasks;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.WebDriver;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import utilities.BaseTest;
+
+import java.time.Duration;
+import java.util.List;
 
 public class Task01 extends BaseTest {
     public static void main(String[] args) {
@@ -25,4 +30,71 @@ public class Task01 extends BaseTest {
         }
         driver.quit();
     }
+
+    @Test
+    public void driverManage() {
+        /*
+         Go to the Amazon URL : https://www.amazon.com/
+         Print the position and size of the page.
+         Adjust the position and size of the page as desired.
+         Test that the page is in the position and size you want.
+          Close the page
+         */
+
+        driver.navigate().to("https://www.amazon.com/");
+        String position = String.valueOf(driver.manage().window().getPosition());
+        System.out.println("Position" + position);
+        String size = String.valueOf(driver.manage().window().getSize());
+        System.out.println("Size " + size);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.manage().window().setPosition(new Point(80, 100));
+        driver.manage().window().setSize(new Dimension(800, 600));
+
+    }
+
+    @Test
+    public void amazon() {
+
+        driver.get("https://www.amazon.com/");
+        WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
+        searchBox.sendKeys("Performance Metrics" + Keys.ENTER);
+        String text1 = driver.getPageSource();
+        System.out.println(text1);
+        searchBox.sendKeys("MEOW" + Keys.ENTER);
+        String text2 = driver.getPageSource();
+        System.out.println(text2);
+        Assert.assertFalse(text1.contains(text2));
+
+    }
+
+    @Test
+    public void googleTitle() {
+        /*
+         Maximize the window.
+• Open google home page https://www.google.com/
+• Verify that you are Google in the title
+         */
+        driver.manage().window().maximize();
+        driver.get("https://www.google.com/");
+        String title = driver.getTitle();
+        System.out.println(title);
+        Assert.assertTrue(title.contains("Google"));
+    }
+
+    @Test
+    public void trendYol() throws InterruptedException {
+
+        driver.get("https://www.trendyol.com/");
+        WebElement searchBox = driver.findElement(By.className("V8wbcUhU"));
+        searchBox.sendKeys("pen"+Keys.ENTER);
+        WebElement pen1= driver.findElement(By.xpath("/html/body/div[1]/div[3]/div[2]/div[2]/div/div/div/div[1]/div[2]/div[4]/div[1]/div/div[1]/div[1]/a/div[1]/div[2]/div[2]"));
+        pen1.click();
+        Thread.sleep(5000);
+        System.out.println(pen1.getSize());
+        Point point=pen1.getLocation();
+        System.out.println("Location "+"x="+point.getX()+"  y="+point.getY());
+
+
+    }
+
 }
--- src/test/java/tasks/Task03.java
+++ src/test/java/tasks/Task03.java
@@ -6,6 +6,7 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -58,5 +59,81 @@ public class Task03 {
         Assert.assertTrue(logoText.contains("com"));
     }
 
+    @Test
+    public void pageSource() {
 
+        driver.get("https://www.amazon.com.tr/");
+        String text = driver.getPageSource();
+        System.out.println("pageSource" + text);
+
+    }
+
+    @Test
+    public void navigation() {
+
+        driver.get("https://www.amazon.com.tr/");
+        WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
+        searchBox.sendKeys("telefon" + Keys.ENTER);
+        driver.navigate().back();
+
+    }
+
+    @Test
+    public void navigationBack() {
+
+        driver.navigate().to("https://www.google.com/");
+        driver.navigate().refresh();
+        driver.navigate().back();
+        driver.navigate().forward();
+
+    }
+
+    @Test
+    public void task() {
+
+        //Invoke FirefoxDriver
+        //Navigate to URL: https://clarusway.com/
+        //Navigate to URL: http://amazon.com/
+        //Come back to the Clarusway using the back command.
+        // Again go back to the amazon website using forward command
+        //Refresh the Browser using refresh command.
+        //Close the Browser.
+
+        driver.navigate().to("https://clarusway.com/");
+        driver.navigate().to("http://amazon.com/");
+        driver.navigate().back();
+        driver.navigate().forward();
+        driver.navigate().refresh();
+
+    }
+
+    @Test
+    public void windowCommands() throws InterruptedException {
+        /*
+        • Invoke Chrome Browser
+• Open URL: https://www.google.com/
+• Maximize the window.
+• Print the position and size of the page.
+• Minimize the page.
+• Wait 5 seconds in the icon state and maximize the page.
+• Print the position and dimensions of the page in maximized state.
+• Make the page fullscreen.
+• Close the Browser
+         */
+
+        driver.get("https://www.google.com/");
+        driver.manage().window().maximize();
+        String size = String.valueOf(driver.manage().window().getSize());
+        System.out.println("Size" + size);
+        String position = String.valueOf(driver.manage().window().getPosition());
+        System.out.println("Position" + position);
+        driver.manage().window().minimize();
+        Thread.sleep(5000);
+        String minSize = String.valueOf(driver.manage().window().getSize());
+        System.out.println("MinSize" + minSize);
+        String minPosition = String.valueOf(driver.manage().window().getPosition());
+        System.out.println("MinPosition");
+        driver.manage().window().fullscreen();
+
+    }
 }
