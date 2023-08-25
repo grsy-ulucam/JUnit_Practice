249abf3e013022aa9beb111491d8b0c089eeedb7
grsy-ulucam <emre.yucel48.45@gmail.com>
2023-08-25 16:25:34 +0300

    25.08.2023

--- src/test/java/tasks/Task05.java
+++ src/test/java/tasks/Task05.java
@@ -18,9 +18,10 @@ public class Task05 {
     //    Go to https://www.linkedin.com/
     //    When we write an e-mail without the @ sign in the e-mail box and press enter.
     //    Let's test the "Invalid email address" warning.
-  WebDriver driver;
+    WebDriver driver;
+
     @Before
-    public void setup(){
+    public void setup() {
         WebDriverManager.firefoxdriver().setup();
         driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
@@ -28,31 +29,32 @@ public class Task05 {
     }
 
     @After
-    public void tearDown(){
+    public void tearDown() {
 
         driver.quit();
     }
 
     @Test
-    public  void test01(){
+    public void test01() {
 
         driver.get("https://www.linkedin.com/");
 
-        WebElement text =driver.findElement(By.xpath("//input[@id='session_key']"));
+        WebElement text = driver.findElement(By.xpath("//input[@id='session_key']"));
 
-        text.sendKeys("zaharyas.com"+ Keys.ENTER);
+        text.sendKeys("zaharyas.com" + Keys.ENTER);
 
-        WebElement password= driver.findElement(By.name("session_password"));
+        WebElement password = driver.findElement(By.name("session_password"));
 
-        password.sendKeys("123344dsfsf"+Keys.ENTER);
+        password.sendKeys("123344dsfsf" + Keys.ENTER);
 
-        WebElement alertMessage= driver.findElement(By.id("error-for-username"));
+        WebElement alertMessage = driver.findElement(By.id("error-for-username"));
 
         Assert.assertTrue(alertMessage.isDisplayed());
 
     }
+
     @Test
-    public  void email() throws InterruptedException {
+    public void email() throws InterruptedException {
        /*
 https://id.heroku.com/login sayfasına gidiniz.
 Bir mail adresi giriniz.
@@ -63,33 +65,52 @@ Eğer yazı görünür değilse "Kayıt Yapıldı" yazdırınız.
 Tüm sayfaları kapatınız.
         */
         driver.get("https://id.heroku.com/login");
-        List<WebElement>list=driver.findElements(By.className("form-control"));
-        WebElement mailBox=list.get(0);
+        List<WebElement> list = driver.findElements(By.className("form-control"));
+        WebElement mailBox = list.get(0);
         mailBox.sendKeys("emre.yucel45.20");
-        WebElement password=list.get(1);
+        WebElement password = list.get(1);
         password.sendKeys("1234545666");
-        WebElement loginButton=driver.findElement(By.cssSelector("#login > form > button"));
+        WebElement loginButton = driver.findElement(By.cssSelector("#login > form > button"));
         loginButton.click();
         Thread.sleep(10000);
-        WebElement errorMesaj=driver.findElement(By.xpath("//*[@id=\"login\"]/form/div[2]/label"));
+        WebElement errorMesaj = driver.findElement(By.xpath("//*[@id=\"login\"]/form/div[2]/label"));
         Assert.assertTrue(errorMesaj.isDisplayed());
-        if (errorMesaj.isDisplayed()){
+        if (errorMesaj.isDisplayed()) {
             System.out.println("Kayıt Yapılamadı");
-        }else System.out.println("Kayıt Yapıldı");
+        } else System.out.println("Kayıt Yapıldı");
     }
+
     @Test
-    public  void calculate(){
+    public void calculate() {
         /*
 Go to URL: https://opensource-demo.orangehrmlive.com/
 Locate the username, password, login button using absolute xpath and relative xpath
          */
 
         driver.navigate().to(" https://opensource-demo.orangehrmlive.com/");
-        WebElement email= driver.findElement(By.xpath("//*[@name='username']"));
+        WebElement email = driver.findElement(By.xpath("//*[@name='username']"));
         email.sendKeys("emre.yucel");
-        WebElement password=driver.findElement(By.xpath("//input[@type='password']"));
+        WebElement password = driver.findElement(By.xpath("//input[@type='password']"));
         password.sendKeys("232342345");
-        WebElement login=driver.findElement(By.xpath("//*[@type='submit']"));
+        WebElement login = driver.findElement(By.xpath("//*[@type='submit']"));
         login.click();
     }
+
+    @Test
+    public void radioButton() {
+
+        driver.get("https://demoqa.com/radio-button");
+        List<WebElement> list = driver.findElements(By.className("custom-control-label"));
+        WebElement yesButton = list.get(0);
+        yesButton.click();
+        Assert.assertTrue(yesButton.getText().contains("Yes"));
+        WebElement ımpressive = list.get(1);
+        ımpressive.click();
+        Assert.assertTrue(ımpressive.getText().contains("Impressive"));
+        WebElement noButton = list.get(2);
+        Assert.assertFalse(noButton.isSelected());
+
+
+    }
+
 }
